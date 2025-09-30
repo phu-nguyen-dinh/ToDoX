fda87a46505000c002744a320775d1eb7e096f93
phunguyendinh <phu0981838248@gmail.com>
2025-09-30 09:10:08 +0700

    feat(frontend): adding shadcn component, NotFound page, file config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,24 @@
-
+import {Toaster, toast} from 'sonner'
+import {BrowserRouter, Routes, Route} from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import NotFound from './pages/NotFound.jsx'
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage/>}
+          />
+          <Route
+            path='*'
+            element={<NotFound/>}
+          />
+        </Routes>
       
+      </BrowserRouter>
     </>
   )
 }
