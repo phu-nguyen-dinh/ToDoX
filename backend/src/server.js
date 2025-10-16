922dc2d0bb53ea3601e722da10ea7498791f27a2
phunguyendinh <phu0981838248@gmail.com>
2025-10-17 00:53:16 +0700

    fix(variable): fixing bug in variable name

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,6 @@ import { connectDB } from "./config/db.js";
 import dotenv from "dotenv"
 import cors from 'cors';
 import path from "path";
-im
 
 dotenv.config();
 const PORT = process.env.PORT || 5001;
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
 
 const api = axios.create({
     baseURL: BASE_URL,
